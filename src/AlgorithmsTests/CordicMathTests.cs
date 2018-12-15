573cd0359a77413ec5cf14dbe7f67c29bbe4dc13
roberthparry <robert.h.parry@gmail.com>
2018-12-15 21:07:52 +0000

    refactor tests

--- src/AlgorithmsTests/CordicMathTests.cs
+++ src/AlgorithmsTests/CordicMathTests.cs
@@ -8,6 +8,7 @@ namespace AlgorithmsTests
     public class CordicMathTests
     {
         private const int _steps = 30;
+        private const double _tolerance = 1.0e-14;
 
         [TestCase(10.0)]
         [TestCase(100.0)]
@@ -17,7 +18,7 @@ namespace AlgorithmsTests
         [TestCase(2.0e-6)]
         public void Ln_Test(double x)
         {
-            Assert.That(CordicMath.Ln(x, _steps)/Math.Log(x), Is.EqualTo(1.0).Within(1.0e-14));
+            Assert.That(CordicMath.Ln(x, _steps)/Math.Log(x), Is.EqualTo(1.0).Within(_tolerance));
         }
 
         [Test]
@@ -26,13 +27,15 @@ namespace AlgorithmsTests
             Assert.Throws<ArgumentException>(() => CordicMath.Ln(0.0));
         }
 
+        [TestCase(-100.0)]
+        [TestCase(100.0)]
         [TestCase(-10.0)]
         [TestCase(10.0)]
         [TestCase(2.0)]
         [TestCase(-2.0)]
         public void Exp_Test(double x)
         {
-            Assert.That(CordicMath.Exp(x, _steps) /Math.Exp(x), Is.EqualTo(1.0).Within(1.0e-14));
+            Assert.That(CordicMath.Exp(x, _steps) /Math.Exp(x), Is.EqualTo(1.0).Within(_tolerance));
         }
     }
 }
