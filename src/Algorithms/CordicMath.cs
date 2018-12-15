70a07c09eb21f1f6855c108db42be26fcf1c0c93
roberthparry <robert.h.parry@gmail.com>
2018-12-15 20:57:43 +0000

    add cordic algorithm for exponent

--- src/Algorithms/CordicMath.cs
+++ src/Algorithms/CordicMath.cs
@@ -12,6 +12,12 @@ namespace Algorithms
             1.0000004768372719, 1.0000002384186075, 1.0000001192092967, 1.0000000596046466, 1.0000000298023228
         };
 
+        /// <summary>
+        ///     Ln compules the Natural Logarithm of x using steps number of steps.
+        /// </summary>
+        /// <returns>The Natural Logarithm.</returns>
+        /// <param name="x">The x value.</param>
+        /// <param name="steps">Steps.</param>
         public static double Ln(double x, uint steps = 25)
         {
             if (x <= 0.0) throw new ArgumentException();
@@ -20,8 +26,17 @@ namespace Algorithms
 
             int k = 0;
 
-            while (Math.E <= x) { k++; x /= Math.E; }
-            while (x < 1.0) { k--; x *= Math.E; }
+            while (x >= Math.E)
+            {
+                k++;
+                x /= Math.E;
+            }
+
+            while (x < 1.0)
+            {
+                k--;
+                x *= Math.E;
+            }
 
             //
             // Determine the weights.
@@ -29,29 +44,97 @@ namespace Algorithms
             var w = new double[steps];
             double ai = 0.0;
 
-            for (int i = 0;  i < steps;  i++)
+            for (int i = 0; i < steps; i++)
             {
                 w[i] = 0.0;
 
-                ai = (i < _a.Length) ? _a[i] : (1.0 + (ai - 1.0) / 2.0);
+                ai = i < _a.Length ? _a[i] : 1.0 + (ai - 1.0) / 2.0;
 
-                if (ai < x) { w[i] = 1.0; x /= ai; }
+                if (ai < x)
+                {
+                    w[i] = 1.0;
+                    x /= ai;
+                }
             }
 
             x -= 1.0;
-            x = x * (1.0 - (x / 2.0) * (1.0 + (x / 3.0) * (1.0 - x / 4.0)));
+            x *= 1.0 - (x / 2.0) * (1.0 + (x / 3.0) * (1.0 - x / 4.0));
 
             //
-            // Assemble.
+            // Assemble
             //
             double poweroftwo = 0.5;
+
             for (int i = 0; i < steps; i++)
             {
                 x += w[i] * poweroftwo;
                 poweroftwo /= 2.0;
             }
 
-            return k + x;
+            return x + (double)k;
+        }
+
+        /// <summary>
+        ///     Compute the Exponent of the value x using steps Steps.
+        /// </summary>
+        /// <returns>The exponent.</returns>
+        /// <param name="x">The x value.</param>
+        /// <param name="steps">Steps.</param>
+        public static double Exp(double x, int steps)
+        {
+            double z;
+
+            int x_int = (int)Math.Floor(x);
+
+            //
+            // Determine the weights.
+            //
+            double poweroftwo = 0.5;
+            z = x - (double)(x_int);
+
+            var w = new double[steps];
+
+            for (int i = 0; i < steps; i++)
+            {
+                w[i] = 0.0;
+                if (poweroftwo < z)
+                {
+                    w[i] = 1.0; ;
+                    z -= poweroftwo;
+                }
+                poweroftwo /= 2.0;
+            }
+            /*
+              Calculate products.
+            */
+            double ai = 0.0;
+            double fx = 1.0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                ai = i < _a.Length ? _a[i] : 1.0 + (ai - 1.0) / 2.0;
+                if (0.0 < w[i]) fx *= ai;
+            }
+
+            //
+            // Perform residual multiplication.
+            //
+            fx *= 1.0 + z * (1.0 + z / 2.0 * (1.0 + z / 3.0 * (1.0 + z / 4.0)));
+
+            //
+            // Account for factor EXP(X_INT).
+            //
+
+            if (x_int < 0)
+            {
+                for (int i = 1; i <= -x_int; i++) fx /= Math.E;
+            }
+            else
+            {
+                for (int i = 1; i <= x_int; i++) fx *= Math.E;
+            }
+
+            return fx;
         }
     }
 }
--- src/AlgorithmsTests/CordicMathTests.cs
+++ src/AlgorithmsTests/CordicMathTests.cs
@@ -15,13 +15,22 @@ namespace AlgorithmsTests
         [TestCase(2.0e-6)]
         public void Ln_Test(double x)
         {
-            Assert.That(CordicMath.Ln(x, 100), Is.EqualTo(Math.Log(x)).Within(1.0e-15));
+            Assert.That(CordicMath.Ln(x, 100)/Math.Log(x), Is.EqualTo(1.0).Within(1.0e-14));
         }
 
         [Test]
         public void Ln_TestInvalidArg()
         {
             Assert.Throws<ArgumentException>(() => CordicMath.Ln(0.0));
+        }
+
+        [TestCase(-10.0)]
+        [TestCase(10.0)]
+        [TestCase(2.0)]
+        [TestCase(-2.0)]
+        public void Exp_Test(double x)
+        {
+            Assert.That(CordicMath.Exp(x, 100)/Math.Exp(x), Is.EqualTo(1.0).Within(1.0e-14));
         }
     }
 }
