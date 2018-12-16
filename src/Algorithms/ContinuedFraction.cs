113c11ea76d08fb10005c20177effb1853802994
roberthparry <robert.h.parry@gmail.com>
2018-12-16 19:37:42 +0000

    add ContinuedFraction

---
+++ src/Algorithms/ContinuedFraction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class ContinuedFraction
+    {
+        /// <summary>
+        ///     Evaluate the continued fraction represented by a0+1/(a1+1/(a2...
+        /// </summary>
+        /// <returns>The evaluated continued fraction.</returns>
+        /// <param name="a">The coefficients of the continued fraction.</param>
+        public static double Evaluate(params double[] a)
+        {
+            if (a == null || a.Length < 1) throw new ArgumentException();
+
+            double h_ = a[0];
+            double k_ = 1.0;
+
+            if (a.Length == 1) return h_;
+
+            double h = a[1] * h_ + 1.0;
+            double k = a[1] * k_;
+
+            for (int j = 2; j < a.Length; j++)
+            {
+                double htemp = h;
+                double ktemp = k;
+                h = a[j] * h + h_;
+                k = a[j] * k + k_;
+                h_ = htemp;
+                k_ = ktemp;
+            }
+
+            return h / k;
+        }
+
+        /// <summary>
+        ///     Evaluate the generalized continued fraction represented by b0 + a0/(b1 + a1/(b2 + a2/(b3...
+        /// </summary>
+        /// <returns>The evaluated continued fraction.</returns>
+        /// <param name="a">The a coefficients.</param>
+        /// <param name="b">The b coefficients.</param>
+        public static double Generalized(double[] a, double[] b)
+        {
+            if (a == null || b == null || b.Length < 1) throw new ArgumentException();
+            if (b.Length != a.Length + 1) throw new ArgumentException();
+
+            double A_ = 1.0;
+            double B_ = 0.0;
+            double A = b[0];
+            double B = 1.0;
+
+            for (int j = 1; j < b.Length; j++)
+            {
+                double Atemp = A;
+                double Btemp = B;
+                A = b[j] * A + a[j - 1] * A_;
+                B = b[j] * B + a[j - 1] * B_;
+                A_ = Atemp;
+                B_ = Btemp;
+            }
+
+            return A / B;
+        }
+    }
+}
---
+++ src/AlgorithmsTests/ContinuedFractionTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using Algorithms;
+using System.Linq;
+
+namespace AlgorithmsTests
+{
+    [TestFixture]
+    public class ContinuedFractionTests
+    {
+        [TestCase(new double[] { 1 }, 1.0)]
+        [TestCase(new double[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, 1.618033985017358)]
+        public void Evaluate_Test(double[] a, double expected)
+        {
+            Assert.That(ContinuedFraction.Evaluate(a), Is.EqualTo(expected).Within(1.0e-14));
+        }
+
+        [Test]
+        public void Evaluate_TestInvalidArg()
+        {
+            Assert.Throws<ArgumentException>(() => ContinuedFraction.Evaluate(new double[] { }));
+            Assert.Throws<ArgumentException>(() => ContinuedFraction.Evaluate(null));
+        }
+
+        [Test]
+        public void Evaluate_TestGoldenRatio()
+        {
+            Assert.That(ContinuedFraction.Evaluate(Enumerable.Repeat(1.0, 40).ToArray()),
+                Is.EqualTo(0.5*(Math.Sqrt(5.0)+1.0)).Within(1.0e-16));
+        }
+
+        [TestCase(new double[] { }, new double[] { 1 }, 1.0)]
+        [TestCase(new double[] { 1, 2, 3 }, new double[] { 1, 2, 3, 4 }, 1.394736842105263157)]
+        [TestCase(new double[] {1,1,1,1,1,1,1,1,1,1,1,1 }, new double[] {1,1,1,1,1,1,1,1,1,1,1,1,1}, 1.6180257510729614)]
+        public void Generalized_Test(double[] a, double[] b, double expected)
+        {
+            Assert.That(ContinuedFraction.Generalized(a, b), Is.EqualTo(expected).Within(1.0e-14));
+        }
+
+        [Test]
+        public void Generalized_TestInvalidArgs()
+        {
+            Assert.Throws<ArgumentException>(() => ContinuedFraction.Generalized(null, null));
+            Assert.Throws<ArgumentException>(() => ContinuedFraction.Generalized(null, new double[] { }));
+            Assert.Throws<ArgumentException>(() => ContinuedFraction.Generalized(new double[] { 1, 1 }, new double[] { }));
+        }
+
+    }
+}
