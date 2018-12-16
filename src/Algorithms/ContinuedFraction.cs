51a1e7952fd4c7a3bb64b39ccfb6d1e93e40d2a4
roberthparry <robert.h.parry@gmail.com>
2018-12-16 19:43:29 +0000

    rename variables

--- src/Algorithms/ContinuedFraction.cs
+++ src/Algorithms/ContinuedFraction.cs
@@ -45,22 +45,22 @@ namespace Algorithms
             if (a == null || b == null || b.Length < 1) throw new ArgumentException();
             if (b.Length != a.Length + 1) throw new ArgumentException();
 
-            double A_ = 1.0;
-            double B_ = 0.0;
-            double A = b[0];
-            double B = 1.0;
+            double h_ = 1.0;
+            double k_ = 0.0;
+            double h = b[0];
+            double k = 1.0;
 
             for (int j = 1; j < b.Length; j++)
             {
-                double Atemp = A;
-                double Btemp = B;
-                A = b[j] * A + a[j - 1] * A_;
-                B = b[j] * B + a[j - 1] * B_;
-                A_ = Atemp;
-                B_ = Btemp;
+                double htemp = h;
+                double ktemp = k;
+                h = b[j] * h + a[j - 1] * h_;
+                k = b[j] * k + a[j - 1] * k_;
+                h_ = htemp;
+                k_ = ktemp;
             }
 
-            return A / B;
+            return h / k;
         }
     }
 }
