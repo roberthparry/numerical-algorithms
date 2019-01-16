a537330f09771bb11e5efcfa75daffc6265cb14f
roberthparry <robert.h.parry@gmail.com>
2019-01-16 08:09:51 +0000

    Add PolyApprox

---
+++ src/Algorithms/PolyApprox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static class PolyApprox
+    {
+        private static readonly double[] _lnCoeffs = new double[] { 
+             };
+
+        private static double Polynomial(double x, params double[] coefficients)
+            => coefficients.Reverse().Aggregate((acc, a) => acc * x + a);
+
+        public static double Ln(double x)
+        {
+            if (x <= 0.0) throw new ArgumentException();
+
+            int k = 0;
+            while (x > 2.0) { k++; x /= Math.E; }
+            while (x < 1.0) { k--; x *= Math.E; }
+            Console.WriteLine($"x = {x}");
+            x -= 1.0;
+            return Polynomial(x, 0.9999964239, -0.4998741238, 0.3317990258, -0.2407338084,
+                                 0.1676540711, -0.0953293897, 0.0360884937, -0.0064535442) * x + (double)k;
+        }
+    }
+}
---
+++ src/AlgorithmsTests/PolyApproxTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Algorithms;
+
+namespace AlgorithmsTests
+{
+    [TestFixture]
+    public class PolyApproxTests
+    {
+        private const double _tolerance = 1.0e-13;
+
+        [TestCase(10.0)]
+        [TestCase(100.0)]
+        [TestCase(1000.0)]
+        [TestCase(10000.0)]
+        [TestCase(2.0)]
+        [TestCase(2.0e-6)]
+        public void Ln_Test(double x)
+        {
+            Assert.That(PolyApprox.Ln(x) / Math.Log(x), Is.EqualTo(1.0).Within(_tolerance));
+        }
+
+        [Test]
+        public void Ln_TestInvalidArg()
+        {
+            Assert.Throws<ArgumentException>(() => PolyApprox.Ln(0.0));
+        }
+    }
+}
